606de6604cbdb9e0fa04d18675f2d49c6e566d60
Asmitha2008 <asmithasr2008@gmail.com>
2026-04-21 16:49:50 +0530

    first commit

---
+++ biodata.py
@@ -0,0 +1,22 @@
+a="Asmitha.S.R"
+b=17
+c="09.07.2008"
+d="Ramesh Aravinth.S.S"
+e="Santhi.M"
+f="12th std"
+g="drawing,cooking,listening music"
+h=95.8
+i="Telugu,Tamil,English"
+j="asmithasr2008@gmail.com"
+k="9876543210"
+print("Name:",a)
+print("Age:",b)
+print("Date of Birth:",c)
+print("Father's Name:",d)
+print("Mother's Name:",e)
+print("Educational Qualification:",f)
+print("Hobbies:",g)
+print("10th Percentage:",h)
+print("Languages Known:",i)
+print("Email ID:",j)
+print("Phone Number:",k)
